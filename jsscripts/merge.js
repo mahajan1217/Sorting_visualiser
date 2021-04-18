5615d0aae16e8f5b0fdfd5f776ce16e57b6f33b4
mahajan1217 <49040430+mahajan1217@users.noreply.github.com>
2021-04-18 22:59:05 +0530

    Update merge.js

--- jsscripts/merge.js
+++ jsscripts/merge.js
@@ -1,27 +1,20 @@
 //let delay = 30;
 async function merge(ele, low, mid, high){
-    console.log('In merge()');
-    console.log(`low=${low}, mid=${mid}, high=${high}`);
     const n1 = mid - low + 1;
     const n2 = high - mid;
-    console.log(`n1=${n1}, n2=${n2}`);
     let left = new Array(n1);
     let right = new Array(n2);
 
     for(let i = 0; i < n1; i++){
         await waitforme(delay);
-        console.log('In merge left loop');
-        console.log(ele[low + i].style.height + ' at ' + (low+i));
         // color
-        ele[low + i].style.background = 'orange';
+        ele[low + i].style.background = 'light red';
         left[i] = ele[low + i].style.height;
     }
     for(let i = 0; i < n2; i++){
         await waitforme(delay);
-        console.log('In merge right loop');
-        console.log(ele[mid + 1 + i].style.height + ' at ' + (mid+1+i));
         // color
-        ele[mid + 1 + i].style.background = 'yellow';
+        ele[mid + 1 + i].style.background = 'red';
         right[i] = ele[mid + 1 + i].style.height;
     }
     await waitforme(delay);
@@ -94,11 +87,9 @@ async function merge(ele, low, mid, high){
 async function mergeSort(ele, l, r){
     console.log('In mergeSort()');
     if(l >= r){
-        console.log(`return cause just 1 elemment l=${l}, r=${r}`);
         return;
     }
     const m = l + Math.floor((r - l) / 2);
-    console.log(`left=${l} mid=${m} right=${r}`, typeof(m));
     await mergeSort(ele, l, m);
     await mergeSort(ele, m + 1, r);
     await merge(ele, l, m, r);
