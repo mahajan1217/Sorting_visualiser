a4b64a5a6e9b1b4ce39fed14f67974c21ba6f2d9
mahajan1217 <49040430+mahajan1217@users.noreply.github.com>
2021-04-18 22:58:48 +0530

    Update insertion.js

--- jsscripts/insertion.js
+++ jsscripts/insertion.js
@@ -1,10 +1,8 @@
 async function insertion(){
-    console.log('In insertion()');
     const ele = document.querySelectorAll(".bar");
     // color
     ele[0].style.background = 'green';
     for(let i = 1; i < ele.length; i++){
-        console.log('In ith loop');
         let j = i - 1;
         let key = ele[i].style.height;
         // color
@@ -13,7 +11,6 @@ async function insertion(){
         await waitforme(delay);
 
         while(j >= 0 && (parseInt(ele[j].style.height) > parseInt(key))){
-            console.log('In while loop');
             // color
             ele[j].style.background = 'blue';
             ele[j + 1].style.height = ele[j].style.height;
