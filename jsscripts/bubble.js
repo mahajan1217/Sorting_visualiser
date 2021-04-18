a1c13044ec6dc7bcbbe883495799e1e5b6d12b06
mahajan1217 <49040430+mahajan1217@users.noreply.github.com>
2021-04-18 22:58:18 +0530

    Update bubble.js

--- jsscripts/bubble.js
+++ jsscripts/bubble.js
@@ -1,19 +1,15 @@
 async function bubble() {
-    console.log('In bubbe()');
     const ele = document.querySelectorAll(".bar");
     for(let i = 0; i < ele.length-1; i++){
-        console.log('In ith loop');
         for(let j = 0; j < ele.length-i-1; j++){
-            console.log('In jth loop');
             ele[j].style.background = 'blue';
             ele[j+1].style.background = 'blue';
             if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height)){
-                console.log('In if condition');
                 await waitforme(delay);
                 swap(ele[j], ele[j+1]);
             }
-            ele[j].style.background = 'cyan';
-            ele[j+1].style.background = 'cyan';
+            ele[j].style.background = 'rgb(12, 92, 241)';
+            ele[j+1].style.background = 'rgb(12, 92, 241)';
         }
         ele[ele.length-1-i].style.background = 'green';
     }
@@ -30,3 +26,4 @@ bubSortbtn.addEventListener('click', async function(){
     enableSizeSlider();
     enableNewArrayBtn();
 });
+
